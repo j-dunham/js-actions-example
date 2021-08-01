202a9f817090f0c97d321648f3e03076e4ab2384
j-dunham <jmdunham20@gmail.com>
2021-07-31 22:26:43 -0400

    fixed import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const github = require('@action/github');
+const github = require('@actions/github');
 
 try {
   const nameToGreet = core.getInput('who-to-greet');
